4b707552fdac0c70ea03b01062f970c43f3e0004
Akoh1 <akohsamuel018@gmail.com>
2022-01-03 01:23:26 +0100

    update config file

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     # SECRET_KEY = '\xec,\x1aK2RA\x05\x04\x89\x04-P\xda\xb9\xec\x04xcm\x8e\xfc\xcf,'
-    # SECRET_KEY = "4@4_#h83h2vp3@9f-c-e1y#-oc*=s4#iwkppvf)9kk6(u3tdj^"
-    SECRET_KEY = config('SECRET_KEY')
+    SECRET_KEY = "4@4_#h83h2vp3@9f-c-e1y#-oc*=s4#iwkppvf)9kk6(u3tdj^"
+    # SECRET_KEY = config('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "./main/database/github_users.db")
 
 class ProductionConfig(Config):
